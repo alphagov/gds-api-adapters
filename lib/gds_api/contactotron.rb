e14aa084480e3e3d5ce6d6196d46cc0d92c98b19
James Stewart <jys@ketlai.co.uk>
2011-11-30 21:54:57 +0000

    Add a sanity test for contactotron. Override initialize on that adapter as we don't want it to take a parameter

--- lib/gds_api/contactotron.rb
+++ lib/gds_api/contactotron.rb
@@ -4,6 +4,9 @@ require_relative 'base'
 # the full URI for a contact and just want to grab its json serialization
 # and convert it to an ostruct.
 class GdsApi::Contactotron < GdsApi::Base
+  def initialize
+  end
+
   def contact_for_uri(uri)
     to_ostruct get_json(uri)
   end
