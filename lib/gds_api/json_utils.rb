ce697cd5b092b2e0f4af929cc6148e5182a7df4f
James Stewart <jys@ketlai.co.uk>
2011-12-03 14:58:53 +0000

    Set a user agent for our http clients

--- lib/gds_api/json_utils.rb
+++ lib/gds_api/json_utils.rb
@@ -4,14 +4,15 @@ require 'ostruct'
 require_relative 'core-ext/openstruct'
 
 module GdsApi::JsonUtils
-
+  USER_AGENT = "GDS Api Client v. #{GdsApi::VERSION}"
+  
   def get_json(url)
     url = URI.parse(url)
     request = url.path
     request = request + "?" + url.query if url.query
 
     response = Net::HTTP.start(url.host, url.port) do |http|
-      http.get(request, {'Accept' => 'application/json'})
+      http.get(request, {'Accept' => 'application/json', 'User-Agent' => USER_AGENT})
     end
     if response.code.to_i != 200
       return nil
@@ -23,7 +24,7 @@ module GdsApi::JsonUtils
   def post_json(url,params)
     url = URI.parse(url)
     Net::HTTP.start(url.host, url.port) do |http|
-      post_response = http.post(url.path, params.to_json, {'Content-Type' => 'application/json'})
+      post_response = http.post(url.path, params.to_json, {'Content-Type' => 'application/json', 'User-Agent' => USER_AGENT})
       if post_response.code == '200'
         return JSON.parse(post_response.body)
       end
