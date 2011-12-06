f7ae5bf75786925cd1f276bc73f0351a060d6c09
Paul Battley <pbattley@gmail.com>
2011-12-06 13:08:35 +0000

    Refactoring. Handle non-string in query params.

--- lib/gds_api/base.rb
+++ lib/gds_api/base.rb
@@ -7,28 +7,30 @@ class GdsApi::Base
   def initialize(platform, endpoint_url = nil)
     adapter_name = self.class.to_s.split("::").last.downcase
 
-    # This should get simpler if we can be more consistent with our domain names
-    self.endpoint =
-      if endpoint_url
-        endpoint_url
-      elsif platform == 'development'
-        "http://#{adapter_name}.dev.gov.uk"
-      else
-        "http://#{adapter_name}.#{platform}.alphagov.co.uk"
-      end
+    self.endpoint = endpoint_url || endpoint_for_platform(adapter_name, platform)
   end
   
   def url_for_slug(slug,options={})
-    base = "#{base_url}/#{slug}.json"
-    return base if options.empty?
-
-    params = options.sort.map { |kv|
-      kv.map { |a| CGI.escape(a) }.join("=")
-    }.join("&")
-
-    "#{base}?#{params}"
+    base = "#{base_url}/#{slug}.json#{query_string(options)}"
   end
-  
-  private
-    attr_accessor :endpoint
+
+private
+  attr_accessor :endpoint
+
+  # This should get simpler if we can be more consistent with our domain names
+  def endpoint_for_platform(adapter_name, platform)
+    if platform == 'development'
+      "http://#{adapter_name}.dev.gov.uk"
+    else
+      "http://#{adapter_name}.#{platform}.alphagov.co.uk"
+    end
+  end
+
+  def query_string(params)
+    return "" if params.empty?
+
+    "?" << params.sort.map { |kv|
+      kv.map { |a| CGI.escape(a.to_s) }.join("=")
+    }.join("&")
+  end
 end
