4c8e69e34b110c57a699d368019401427a93213b
James Stewart <jys@ketlai.co.uk>
2011-12-07 15:52:14 +0000

    Allow retrieval of panopticon details as a hash

--- lib/gds_api/panopticon.rb
+++ lib/gds_api/panopticon.rb
@@ -2,10 +2,15 @@ require_relative 'base'
 
 class GdsApi::Panopticon < GdsApi::Base
 
-  def artefact_for_slug(slug)
+  def artefact_for_slug(slug, opts = {})
     return nil if slug.nil? or slug == ''
-
-    to_ostruct get_json(url_for_slug(slug))
+    
+    details = get_json(url_for_slug(slug))
+    if opts[:as_hash]
+      details
+    else
+      to_ostruct(details) 
+    end
   end
 
 private
