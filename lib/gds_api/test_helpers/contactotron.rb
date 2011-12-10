fce782749f61ee79762845d893516e14798f68e7
James Stewart <jys@ketlai.co.uk>
2011-12-10 14:09:28 +0000

    Add GdsApi::TestHelpers::Contactotron and make our contactotron tests use them

---
+++ lib/gds_api/test_helpers/contactotron.rb
@@ -0,0 +1,12 @@
+module GdsApi
+  module TestHelpers
+    module Contactotron
+        
+      def contactotron_has_contact(uri, details)
+        json = JSON.dump(details)
+        stub_request(:get, uri).to_return(:status => 200, :body => json, :headers => {})
+        return uri
+      end
+    end
+  end
+end
--- test/contactotron_api_test.rb
+++ test/contactotron_api_test.rb
@@ -1,15 +1,17 @@
 require 'test_helper'
 require 'gds_api/contactotron'
+require 'gds_api/test_helpers/contactotron'
 
 class ContactotronApiTest < MiniTest::Unit::TestCase
+  include GdsApi::TestHelpers::Contactotron
+  
   def api
     GdsApi::Contactotron.new
   end
 
   def test_should_fetch_and_parse_JSON_into_ostruct
-    uri = "http://contactotron.platform/contacts/1"
-    stub_request(:get, uri).
-      to_return(:status => 200, :body => '{"detail":"value"}')
+    uri = "http://contactotron.platform/contacts/1"    
+    contactotron_has_contact(uri, {details: 'value'})
     assert_equal OpenStruct, api.contact_for_uri(uri).class
   end
 end
