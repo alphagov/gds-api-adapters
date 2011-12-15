d2f0eacab5006101910b9f9389c9435578bb41b0
James Stewart <jys@ketlai.co.uk>
2011-12-15 21:02:44 +0000

    Several more test helpers extracted from frontend

--- lib/gds_api/test_helpers/publisher.rb
+++ lib/gds_api/test_helpers/publisher.rb
@@ -9,6 +9,33 @@ module GdsApi
         stub_request(:get, uri).to_return(:body => json, :status => 200)
         return uri
       end
+
+      def publication_exists_for_snac(snac, details)
+        json = JSON.dump(details)
+        uri = "#{PUBLISHER_ENDPOINT}/publications/#{details['slug']}.json"
+        stub_request(:get, uri).with(:query => {:snac => snac.to_s}).to_return(:body => json, :status => 200)
+        return uri
+      end
+
+      def publication_does_not_exist(details)
+        uri = "#{PUBLISHER_ENDPOINT}/publications/#{details['slug']}.json"
+        stub_request(:get, uri).to_return(:body => 'Not Found', :status => 404)
+        return uri
+      end
+
+      def council_exists_for_slug(input_details, output_details)
+        json = JSON.dump(output_details)
+        slug = input_details.delete('slug')
+        uri = "#{PUBLISHER_ENDPOINT}/local_transactions/#{slug}/verify_snac.json"
+        stub_request(:post, uri).with(:body => JSON.dump(input_details), 
+          :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/json', 'User-Agent'=>'GDS Api Client v. 0.0.7'}).
+          to_return(:body => json, :status => 200)
+      end
+
+      def no_council_for_slug(slug)
+        uri = "#{PUBLISHER_ENDPOINT}/local_transactions/#{slug}/verify_snac.json"
+        stub_request(:post, uri).to_return(:body => "", :status => 404)
+      end
     end
   end
 end
