e0d2fe9af19d1599b407ec3bb538ba5e01fdfb05
James Stewart <jys@ketlai.co.uk>
2011-12-15 21:04:09 +0000

    First imminence test helper extracted from frontend

---
+++ lib/gds_api/test_helpers/imminence.rb
@@ -0,0 +1,12 @@
+module GdsApi
+  module TestHelpers
+    module Imminence
+      def imminence_has_places(latitude, longitude, details)
+        response = JSON.dump(details['details'])
+        stub_request(:get, "http://imminence.test.alphagov.co.uk/places/#{details['slug']}.json").
+          with(:query => {"lat" => latitude, "lng" => longitude, "limit" => "5"}).
+          to_return(:status => 200, :body => response, :headers => {})
+      end
+    end
+  end
+end
