0a3b0a4f475243587f13837906d9a546461106b8
James Stewart <jys@ketlai.co.uk>
2011-12-16 11:16:36 +0000

    Add fetch_artefact as a helper so it's easier to use consistently across apps

--- lib/gds_api/helpers.rb
+++ lib/gds_api/helpers.rb
@@ -16,6 +16,10 @@ module GdsApi
       @panopticon_api ||= GdsApi::Panopticon.new(Plek.current.environment)
     end
     
+    def fetch_artefact(params)
+      panopticon_api.artefact_for_slug(params[:slug]) || OpenStruct.new(section: 'missing', need_id: 'missing', kind: 'missing')
+    end
+
     def self.included(klass)
       if klass.respond_to?(:helper_method)
         klass.helper_method :publisher_api, :panopticon_api, :imminence_api
