2062b0ca2cf96e87cd5a1da326d1d7642f583402
James Stewart <jys@ketlai.co.uk>
2011-12-20 20:12:05 +0000

    Add pending test for timeout handling so others can have a go at making it work

--- test/json_utils_test.rb
+++ test/json_utils_test.rb
@@ -1,5 +1,23 @@
+require_relative 'test_helper'
+require 'gds_api/json_utils'
+require 'rack'
+
+StubRackApp = lambda do |env|
+  sleep(30)
+  body = '{"some":"value"}'
+  [200, {"Content-Type" => "text/plain", "Content-Length" => body.length.to_s}, [body]]
+end
+
 class JsonUtilsTest < MiniTest::Unit::TestCase
   include GdsApi::JsonUtils
+
+  def pending_test_long_requests_timeout
+    url = "http://www.example.com/timeout.json"
+    stub_request(:get, url).to_rack(StubRackApp)
+    assert_raises GdsApi::TimedOut do
+      get_json(url)
+    end
+  end
 
   def test_get_returns_nil_on_timeout
     url = "http://some.endpoint/some.json"
