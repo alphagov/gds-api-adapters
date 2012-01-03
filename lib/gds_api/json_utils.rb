3abf23399ec711e448f1dd99d70e46844ad5b8e2
James Stewart <jys@ketlai.co.uk>
2012-01-03 10:01:18 +0000

    Don't worry about verifying SSL certificates
    We need this so preview will work until we get a proper cert there

--- lib/gds_api/json_utils.rb
+++ lib/gds_api/json_utils.rb
@@ -22,6 +22,7 @@ module GdsApi::JsonUtils
     logger.debug "I will request #{request}"
 
     response = Net::HTTP.start(url.host, url.port, nil, nil, nil, nil, {use_ssl: url.port == 443}) do |http|
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE if url.port == 443
       http.read_timeout = options[:timeout] || DEFAULT_TIMEOUT_IN_SECONDS
       yield http, request
     end
