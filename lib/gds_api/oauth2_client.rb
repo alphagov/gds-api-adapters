821894d49af7f0d61e39e7c8dd500a723472a90f
David Heath <david@davidheath.org>
2012-01-09 11:03:20 +0000

    experimental (not working) oauth2 client.

---
+++ lib/gds_api/oauth2_client.rb
@@ -0,0 +1,25 @@
+require_relative 'response'
+require_relative 'exceptions'
+
+# TODO: This is a work in progress
+module GdsApi
+  class OAuth2Client
+    def initialize(options)
+      raise "access_token required" unless options[:access_token]
+      @oauth_client = OAuth2::Client.new(nil, nil)
+      @access_token = OAuth2::AccessToken.new(@oauth_client, options[:access_token], options[:token_options])
+    end
+    
+    def get_json(url)
+      @access_token.get(url, headers: REQUEST_HEADERS)
+    end
+
+    def post_json(url, params)
+      @access_token.post(url, body: params, headers: REQUEST_HEADERS)
+    end
+
+    def put_json(url, params)
+      @access_token.put(url, body: params, headers: REQUEST_HEADERS)
+    end
+  end
+end
