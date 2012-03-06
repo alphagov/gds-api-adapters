a240bae695578e40ef170f541d4fc15a4beaf88a
Jordan Hatch <jordan@jordanh.net>
2012-03-06 13:54:32 +0000

    Stub both HTTP and HTTPS requests to Imminence in the test environment

--- lib/gds_api/test_helpers/imminence.rb
+++ lib/gds_api/test_helpers/imminence.rb
@@ -8,6 +8,10 @@ module GdsApi
         stub_request(:get, "http://imminence.test.alphagov.co.uk/places/#{details['slug']}.json").
           with(:query => {"lat" => latitude, "lng" => longitude, "limit" => "5"}).
           to_return(:status => 200, :body => response, :headers => {})
+
+        stub_request(:get, "https://imminence.test.alphagov.co.uk/places/#{details['slug']}.json").
+          with(:query => {"lat" => latitude, "lng" => longitude, "limit" => "5"}).
+            to_return(:status => 200, :body => response, :headers => {})
       end
     end
   end
