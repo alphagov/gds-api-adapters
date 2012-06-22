f92cadb5e2010b8897baa08f1e0ca9ea0c80e6d5
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-06-22 10:07:49 +0100

    Switch publisher tests to use Minitest::Spec syntax

--- test/publisher_api_test.rb
+++ test/publisher_api_test.rb
@@ -3,7 +3,7 @@ require 'gds_api/publisher'
 require 'gds_api/json_client'
 require 'gds_api/test_helpers/publisher'
 
-class GdsApi::PublisherTest < MiniTest::Unit::TestCase
+describe GdsApi::Publisher do
   include GdsApi::TestHelpers::Publisher
 
   def basic_answer
@@ -54,14 +54,14 @@ class GdsApi::PublisherTest < MiniTest::Unit::TestCase
     GdsApi::Publisher.new("test")
   end
 
-  def test_given_a_slug_should_go_get_resource_from_publisher_app
+  it "should go get resource from publisher app given a slug" do
     publication_exists(basic_answer)
     pub = api.publication_for_slug(basic_answer['slug'])
 
     assert_equal "Something", pub.body
   end
 
-  def test_should_optionally_accept_an_edition_id
+  it "should optionally accept an edition id" do
     slug = "a-publication"
     publication = %@{"audiences":[""],
       "slug":"#{slug}",
@@ -76,58 +76,60 @@ class GdsApi::PublisherTest < MiniTest::Unit::TestCase
     pub = api.publication_for_slug(slug,{:edition => 678})
   end
 
-  def test_should_construct_correct_url_for_a_slug
+  it "should construct correct url for a slug" do
     assert_equal "#{PUBLISHER_ENDPOINT}/publications/slug.json", api.url_for_slug("slug")
   end
 
-  def test_parts_should_be_deserialised_into_whole_objects
+  it "should deserialise parts into whole objects" do
     publication_exists(publication_with_parts)
     pub = api.publication_for_slug(publication_with_parts['slug'])
     assert_equal 3, pub.parts.size
     assert_equal "introduction", pub.parts.first.slug
   end
 
-  def test_a_publication_with_parts_should_have_part_specific_methods
+  it "should have part specific methods for a publication with parts" do
     publication_exists(publication_with_parts)
     pub = api.publication_for_slug(publication_with_parts['slug'])
     assert_equal pub.part_index("introduction"),0
   end
 
-  def test_updated_at_should_be_a_time_on_deserialisation
+  it "should deserialise updated at as a time" do
     publication_exists(publication_with_parts)
     pub = api.publication_for_slug(publication_with_parts['slug'])
     assert_equal Time, pub.updated_at.class
   end
 
-  def test_should_be_able_to_retrieve_local_transaction_details
+  it "should be able to retrieve local transaction details" do
     stub_request(:post, "#{PUBLISHER_ENDPOINT}/local_transactions/fake-transaction/verify_snac.json").
       with(:body => "{\"snac_codes\":[12345]}", :headers => GdsApi::JsonClient::REQUEST_HEADERS).
       to_return(:status => 200, :body => '{"snac": "12345"}', :headers => {})
     assert_equal '12345', api.council_for_slug('fake-transaction', [12345])
   end
 
-  def test_should_get_licence_details_from_publisher
-    setup_publisher_licences_stubs
+  describe "getting licence details from publisher" do
+    it "should get licence details from publisher" do
+      setup_publisher_licences_stubs
 
-    publisher_has_licence :licence_identifier => "1234", :title => 'Test Licence 1', :slug => 'test-licence-1', 
-      :licence_short_description => 'A short description'
-    publisher_has_licence :licence_identifier => "1235", :title => 'Test Licence 2', :slug => 'test-licence-2', 
-      :licence_short_description => 'A short description'
-    publisher_has_licence :licence_identifier => "AB1234", :title => 'Test Licence 3', :slug => 'test-licence-3', 
-      :licence_short_description => 'A short description'
+      publisher_has_licence :licence_identifier => "1234", :title => 'Test Licence 1', :slug => 'test-licence-1', 
+        :licence_short_description => 'A short description'
+      publisher_has_licence :licence_identifier => "1235", :title => 'Test Licence 2', :slug => 'test-licence-2', 
+        :licence_short_description => 'A short description'
+      publisher_has_licence :licence_identifier => "AB1234", :title => 'Test Licence 3', :slug => 'test-licence-3', 
+        :licence_short_description => 'A short description'
 
-    results = api.licences_for_ids([1234, 'AB1234', 'something'])
-    assert_equal 2, results.size
-    assert_equal ['1234', 'AB1234'], results.map(&:licence_identifier)
-    assert_equal ['Test Licence 1', 'Test Licence 3'], results.map(&:title).sort
-    assert_equal ['test-licence-1', 'test-licence-3'], results.map(&:slug).sort
-    assert_equal 'A short description', results[0].licence_short_description
-    assert_equal 'A short description', results[1].licence_short_description
-  end
+      results = api.licences_for_ids([1234, 'AB1234', 'something'])
+      assert_equal 2, results.size
+      assert_equal ['1234', 'AB1234'], results.map(&:licence_identifier)
+      assert_equal ['Test Licence 1', 'Test Licence 3'], results.map(&:title).sort
+      assert_equal ['test-licence-1', 'test-licence-3'], results.map(&:slug).sort
+      assert_equal 'A short description', results[0].licence_short_description
+      assert_equal 'A short description', results[1].licence_short_description
+    end
 
-  def test_should_return_empty_array_with_no_licences
-    setup_publisher_licences_stubs
+    it "should return empty array with no licences" do
+      setup_publisher_licences_stubs
 
-    assert_equal [], api.licences_for_ids([123,124])
+      assert_equal [], api.licences_for_ids([123,124])
+    end
   end
 end
