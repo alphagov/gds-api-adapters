23f9fcb9aad34ac81d9d6f341fb9ba19c8155281
Kushal Pisavadia <kushi.p@gmail.com>
2012-08-06 05:33:48 +0100

    Remove http|https stub duplication
    Simplify two (near identical) blocks to a single block which is fed in
    the protocol.

--- lib/gds_api/test_helpers/imminence.rb
+++ lib/gds_api/test_helpers/imminence.rb
@@ -5,13 +5,12 @@ module GdsApi
     module Imminence
       def imminence_has_places(latitude, longitude, details)
         response = JSON.dump(details['details'])
-        stub_request(:get, "http://imminence.test.alphagov.co.uk/places/#{details['slug']}.json").
+
+        ["http", "https"].each { |protocol|
+          stub_request(:get, "#{protocol}://imminence.test.alphagov.co.uk/places/#{details['slug']}.json").
           with(:query => {"lat" => latitude, "lng" => longitude, "limit" => "5"}).
           to_return(:status => 200, :body => response, :headers => {})
-
-        stub_request(:get, "https://imminence.test.alphagov.co.uk/places/#{details['slug']}.json").
-          with(:query => {"lat" => latitude, "lng" => longitude, "limit" => "5"}).
-            to_return(:status => 200, :body => response, :headers => {})
+        }
       end
     end
   end
