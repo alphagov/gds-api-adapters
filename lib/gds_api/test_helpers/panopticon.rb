a9dfa5d45675f4b3161a57a9859c1ce7673a80bd
John Griffin <johnog@gmail.com>
2012-08-30 09:41:05 +0100

    Keys do not need to be converted strings before being dumped to JSON

--- lib/gds_api/test_helpers/panopticon.rb
+++ lib/gds_api/test_helpers/panopticon.rb
@@ -5,21 +5,14 @@ module GdsApi
     module Panopticon
       PANOPTICON_ENDPOINT = 'https://panopticon.test.alphagov.co.uk'
 
-      def stringify_hash_keys(input_hash)
-        input_hash.inject({}) do |options, (key, value)|
-          options[key.to_s] = value
-          options
-        end
-      end
-
       def panopticon_has_metadata(metadata)
-        metadata = stringify_hash_keys(metadata)
-
         json = JSON.dump(metadata)
 
         urls = []
-        urls << "#{PANOPTICON_ENDPOINT}/artefacts/#{metadata['id']}.json" if metadata['id']
-        urls << "#{PANOPTICON_ENDPOINT}/artefacts/#{metadata['slug']}.json" if metadata['slug']
+        id   = metadata[:id]
+        slug = metadata[:slug]
+        urls << "#{PANOPTICON_ENDPOINT}/artefacts/#{id}.json"   if id
+        urls << "#{PANOPTICON_ENDPOINT}/artefacts/#{slug}.json" if slug
 
         urls.each { |url| stub_request(:get, url).to_return(:status => 200, :body => json, :headers => {}) }
 
