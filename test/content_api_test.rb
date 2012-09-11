b0bfb47a6399f6342d272696de5a6f3cd8fb1b74
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-09-11 15:05:17 +0000

    Content API tests use specs form
    To make the tests easier to understand, and to better scope each set of
    tests by major URL group use the spec structure that MiniTest provides
    instead of the basic unit test structure.

--- test/content_api_test.rb
+++ test/content_api_test.rb
@@ -2,29 +2,33 @@ require 'test_helper'
 require 'gds_api/content_api'
 require 'gds_api/test_helpers/content_api'
 
-class ContentApiTest < MiniTest::Unit::TestCase
+describe GdsApi::ContentApi do
   include GdsApi::TestHelpers::ContentApi
 
-  def api
-    GdsApi::ContentApi.new('test')
+  before do
+    @api = GdsApi::ContentApi.new('test')
   end
 
-  def test_sections
-    content_api_has_root_sections(["crime"])
-    response = api.sections
-    first_section = response["results"][0]
-    assert_equal "http://contentapi.test.gov.uk/tags/crime.json", first_section["id"]
+  describe "sections" do
+    it "should show a list of sections" do
+      content_api_has_root_sections(["crime"])
+      response = @api.sections
+      first_section = response["results"][0]
+      assert_equal "http://contentapi.test.gov.uk/tags/crime.json", first_section["id"]
+    end
   end
 
-  def test_with_tag
-    tag = "crime-and-justice"
-    api_url = "https://contentapi.test.alphagov.co.uk/with_tag.json?tag=#{tag}&include_children=1"
-    json = {
-      results: [{title: "Complain about a claims company"}]
-    }.to_json
-    stub_request(:get, api_url).to_return(:status => 200, :body => json)
-    response = api.with_tag("crime-and-justice")
-    subsection = response["results"][0]
-    assert_equal "Complain about a claims company", subsection["title"]
+  describe "tags" do
+    it "should produce an artefact with the provided tag" do
+      tag = "crime-and-justice"
+      api_url = "https://contentapi.test.alphagov.co.uk/with_tag.json?tag=#{tag}&include_children=1"
+      json = {
+        results: [{title: "Complain about a claims company"}]
+      }.to_json
+      stub_request(:get, api_url).to_return(:status => 200, :body => json)
+      response = @api.with_tag("crime-and-justice")
+      subsection = response["results"][0]
+      assert_equal "Complain about a claims company", subsection["title"]
+    end
   end
 end
