f6c352a74073c5d684084a06a5d6871532399a44
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-09-14 10:01:25 +0000

    Return a GdsApi::Response instead of a hash
    Where possible, don't swallow errors wholesale. Instead return a
    GdsApi::Response object so that any server errors can be handled
    correctly in the future.
    
    Bubble these out to the clients that are using them.

--- lib/gds_api/licence_application.rb
+++ lib/gds_api/licence_application.rb
@@ -2,21 +2,12 @@ require_relative "base"
 
 class GdsApi::LicenceApplication < GdsApi::Base
   def all_licences
-    if response = get_json("#{@endpoint}/api/licences")
-      response.to_hash
-    else
-      nil
-    end
+    get_json!("#{@endpoint}/api/licences")
   end
 
   def details_for_licence(id, snac_code = nil)
     return nil if id.nil?
-
-    if response = get_json(build_licence_url(id, snac_code))
-      response.to_hash
-    else
-      nil
-    end
+    get_json!(build_licence_url(id, snac_code))
   end
 
   private
