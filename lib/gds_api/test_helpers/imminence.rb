3477643544b133f5cad64a224c0d9632caee9038
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-09-27 09:40:55 +0000

    Add test helper for business support schemes

--- lib/gds_api/test_helpers/imminence.rb
+++ lib/gds_api/test_helpers/imminence.rb
@@ -3,14 +3,22 @@ require 'gds_api/test_helpers/json_client_helper'
 module GdsApi
   module TestHelpers
     module Imminence
+      IMMINENCE_API_HOST = "imminence.test.alphagov.co.uk"
       def imminence_has_places(latitude, longitude, details)
         response = JSON.dump(details['details'])
 
         ["http", "https"].each do |protocol|
-          stub_request(:get, "#{protocol}://imminence.test.alphagov.co.uk/places/#{details['slug']}.json").
+          stub_request(:get, "#{protocol}://#{IMMINENCE_API_HOST}/places/#{details['slug']}.json").
           with(:query => {"lat" => latitude, "lng" => longitude, "limit" => "5"}).
           to_return(:status => 200, :body => response, :headers => {})
         end
+      end
+
+      def imminence_has_business_support_schemes(facets_hash, schemes)
+        response = schemes.to_json(only: [:business_support_identifier, :title])
+        stub_request(:get, "https://#{IMMINENCE_API_HOST}/business_support_schemes.json").
+        with(query: facets_hash).
+        to_return(status: 200, body: response, headers: {})
       end
     end
   end
