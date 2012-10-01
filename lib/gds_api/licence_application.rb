13e40dfc84f2a659271b195c4e5dd9862b42b969
Jordan Hatch <jordan@jordanh.net>
2012-10-01 16:39:26 +0000

    Update the Licensify API endpoint.
    Due to the Licensify API endpoint being moved, we'll need to update
    the URL we're pointing at in API adapters.

--- lib/gds_api/licence_application.rb
+++ lib/gds_api/licence_application.rb
@@ -2,7 +2,7 @@ require_relative "base"
 
 class GdsApi::LicenceApplication < GdsApi::Base
   def all_licences
-    get_json!("#{@endpoint}/api/licences")
+    get_json!("#{@endpoint}/apply-for-a-licence/api/licences")
   end
 
   def details_for_licence(id, snac_code = nil)
@@ -18,9 +18,9 @@ class GdsApi::LicenceApplication < GdsApi::Base
 
   def build_licence_url(id, snac_code)
     if snac_code
-      "#{@endpoint}/api/licence/#{id}/#{snac_code}"
+      "#{@endpoint}/apply-for-a-licence/api/licence/#{id}/#{snac_code}"
     else
-      "#{@endpoint}/api/licence/#{id}"
+      "#{@endpoint}/apply-for-a-licence/api/licence/#{id}"
     end
   end
 end
