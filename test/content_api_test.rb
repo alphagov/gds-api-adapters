b8db2dda58cf43861b7bb19d0ab905143eea642c
James Stewart <jys@ketlai.co.uk>
2012-10-06 12:54:00 +0100

    Update setup_content_api_licences_stubs to provide more faithful response
    Where this is used it should return something that actually
    looks like a Content API response.

--- test/content_api_test.rb
+++ test/content_api_test.rb
@@ -369,19 +369,19 @@ describe GdsApi::ContentApi do
       content_api_has_licence :licence_identifier => "AB1234", :title => 'Test Licence 3', :slug => 'test-licence-3',
         :licence_short_description => 'A short description'
 
-      results = @api.licences_for_ids([1234, 'AB1234', 'something']).to_ostruct
+      results = @api.licences_for_ids([1234, 'AB1234', 'something']).to_ostruct.results
       assert_equal 2, results.size
-      assert_equal ['1234', 'AB1234'], results.map(&:licence_identifier)
+      assert_equal ['1234', 'AB1234'], results.map { |r| r.details.licence_identifier }
       assert_equal ['Test Licence 1', 'Test Licence 3'], results.map(&:title).sort
-      assert_equal ['test-licence-1', 'test-licence-3'], results.map(&:slug).sort
-      assert_equal 'A short description', results[0].licence_short_description
-      assert_equal 'A short description', results[1].licence_short_description
+      assert_equal ['http://www.test.gov.uk/test-licence-1', 'http://www.test.gov.uk/test-licence-3'], results.map(&:web_url).sort
+      assert_equal 'A short description', results[0].details.licence_short_description
+      assert_equal 'A short description', results[1].details.licence_short_description
     end
 
     it "should return empty array with no licences" do
       setup_content_api_licences_stubs
 
-      assert_equal [], @api.licences_for_ids([123,124]).to_ostruct
+      assert_equal [], @api.licences_for_ids([123,124]).to_ostruct.results
     end
 
     it "should raise an error if publisher returns an error" do
