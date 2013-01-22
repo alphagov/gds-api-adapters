039b107cb0cb482d4d7fa4a8521e03e8dfd1eeb8
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-01-22 18:44:38 +0000

    Use RestClient internally for redirect following.
    The main difficulties here stemmed from the way the logging code
    is interleaved throughout the code and had to be passed around
    between methods, which doesn't work with RestClient's more
    exception-based handling of HTTP error codes. Also, some chunky
    refactoring because Net::HTTP lets you modify a request object
    before sending it, while RestClient implements this as arguments
    to the Request#execute method.
    
    There's also quite a lot of weirdness around the do_raw_request
    method, which works differently from do_json_request in that it
    ignores any HTTP error codes and returns the body of the response.
    Since there aren't any tests, I can't tell whether this was the
    intended behaviour, but since the only use I know of for this is
    fetching KML data from Imminence, which we no longer do, I'm not
    so worried about breaking backwards compatibility in error cases.

--- lib/gds_api/json_client.rb
+++ lib/gds_api/json_client.rb
@@ -1,8 +1,8 @@
 require_relative 'response'
 require_relative 'exceptions'
 require_relative 'version'
-require 'net/http'
 require 'lrucache'
+require 'rest-client'
 
 module GdsApi
   class JsonClient
@@ -37,7 +37,9 @@ module GdsApi
     DEFAULT_CACHE_TTL = 15 * 60 # 15 minutes
 
     def get_raw(url)
-      do_raw_request(Net::HTTP::Get, url)
+      ignoring GdsApi::HTTPNotFound do
+        do_raw_request(:get, url)
+      end
     end
 
     # Define "safe" methods for each supported HTTP method
@@ -55,108 +57,132 @@ module GdsApi
     end
 
     def get_json!(url, &create_response)
-      @cache[url] ||= do_json_request(Net::HTTP::Get, url, nil, &create_response)
+      @cache[url] ||= do_json_request(:get, url, nil, &create_response)
     end
 
     def post_json!(url, params)
-      do_json_request(Net::HTTP::Post, url, params)
+      do_json_request(:post, url, params)
     end
 
     def put_json!(url, params)
-      do_json_request(Net::HTTP::Put, url, params)
+      do_json_request(:put, url, params)
     end
 
     def delete_json!(url, params = nil)
-      do_request(Net::HTTP::Delete, url, params)
+      do_request(:delete, url, params)
     end
 
     private
-    def do_raw_request(method_class, url, params = nil)
-      response, loggable = do_request(method_class, url, params)
+    def do_raw_request(method, url, params = nil)
+      response = do_request(method, url, params)
       response.body
+
+    rescue RestClient::ResourceNotFound => e
+      raise GdsApi::HTTPNotFound.new(e.http_code)
+
+    rescue RestClient::Exception => e
+      raise GdsApi::HTTPErrorResponse.new(response.code.to_i), e.response.body
     end
 
-    # method_class: the Net::HTTP class to use, e.g. Net::HTTP::Get
+    # method: the symbolic name of the method to use, e.g. :get, :post
     # url:    the request URL
     # params: the data to send (JSON-serialised) in the request body
     # create_response: optional block to instantiate a custom response object
     #                  from the Net::HTTPResponse
-    def do_json_request(method_class, url, params = nil, &create_response)
+    def do_json_request(method, url, params = nil, &create_response)
 
-      response, loggable = do_request(method_class, url, params)
+      begin
+        response = do_request(method, url, params)
+
+      rescue RestClient::ResourceNotFound => e
+        raise GdsApi::HTTPNotFound.new(e.http_code)
+
+      rescue RestClient::Exception => e
+        # Attempt to parse the body as JSON if possible
+        body = begin
+          JSON.parse(e.http_body)
+        rescue JSON::ParserError
+          e.response.body
+        end
+        raise GdsApi::HTTPErrorResponse.new(e.http_code), body
+      end
 
       # If no custom response is given, just instantiate Response
       create_response ||= Proc.new { |r| Response.new(r) }
-
-      if response.is_a?(Net::HTTPSuccess)
-        logger.info loggable.merge(status: 'success', end_time: Time.now.to_f).to_json
-        create_response.call(response)
-      elsif response.is_a?(Net::HTTPNotFound)
-        raise GdsApi::HTTPNotFound.new(response.code.to_i)
-      else
-        body = begin
-          JSON.parse(response.body.to_s)
-        rescue JSON::ParserError
-          response.body
-        end
-        loggable.merge!(status: response.code, end_time: Time.now.to_f, body: body)
-        logger.warn loggable.to_json
-        raise GdsApi::HTTPErrorResponse.new(response.code.to_i), body
-      end
+      create_response.call(response.net_http_res)
     end
 
-    def extract_url_and_path(url)
-      url = URI.parse(url)
-      path = url.path
-      path = path + "?" + url.query if url.query
-      return url, path
-    end
-
-    def attach_auth_options(request)
+    # Take a hash of parameters for Request#execute; return a hash of
+    # parameters with authentication information included
+    def with_auth_options(method_params)
       if @options[:bearer_token]
-        request.add_field('Authorization', "Bearer #{@options[:bearer_token]}")
+        headers = method_params[:headers] || {}
+        method_params.merge(headers: headers.merge(
+          {"Authorization" => "Bearer #{@options[:bearer_token]}"}
+        ))
       elsif @options[:basic_auth]
-        request.basic_auth(@options[:basic_auth][:user], @options[:basic_auth][:password])
-      end
-    end
-
-    def set_timeout(http)
-      unless options[:disable_timeout]
-        http.read_timeout = options[:timeout] || DEFAULT_TIMEOUT_IN_SECONDS
-      end
-    end
-
-    def ssl_options(port)
-      if port == 443
-        {use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE}
+        method_params.merge(
+          user: @options[:basic_auth][:user],
+          password: @options[:basic_auth][:password]
+        )
       else
-        {}
+        method_params
       end
     end
 
-    def do_request(method_class, url, params = nil)
+    # Take a hash of parameters for Request#execute; return a hash of
+    # parameters with timeouts included
+    def with_timeout(method_params)
+      if options[:disable_timeout]
+        method_params.merge(timeout: -1)
+      else
+        method_params.merge(
+          timeout: options[:timeout] || DEFAULT_TIMEOUT_IN_SECONDS
+        )
+      end
+    end
+
+    def with_ssl_options(method_params)
+      method_params.merge(
+        # This is the default value anyway, but we should probably be explicit
+        verify_ssl: OpenSSL::SSL::VERIFY_NONE
+      )
+    end
+
+    def do_request(method, url, params = nil)
       loggable = {request_uri: url, start_time: Time.now.to_f}
       start_logging = loggable.merge(action: 'start')
       logger.debug start_logging.to_json
 
-      url, path = extract_url_and_path(url)
-
-      response = Net::HTTP.start(url.host, url.port, nil, nil, nil, nil, ssl_options(url.port)) do |http|
-        set_timeout(http)
-        request = method_class.new(path, DEFAULT_REQUEST_HEADERS)
-        attach_auth_options(request)
-        request.body = params.to_json if params
-        http.request(request)
+      method_params = {
+        method: method,
+        url: url,
+        headers: DEFAULT_REQUEST_HEADERS
+      }
+      method_params[:payload] = params.to_json if params
+      method_params = with_auth_options(method_params)
+      method_params = with_timeout(method_params)
+      if URI.parse(url).is_a? URI::HTTPS
+        method_params = with_ssl_options(method_params)
       end
 
-      return response, loggable
+      return ::RestClient::Request.execute(method_params)
 
     rescue Errno::ECONNREFUSED => e
       logger.error loggable.merge(status: 'refused', error_message: e.message, error_class: e.class.name, end_time: Time.now.to_f).to_json
       raise GdsApi::EndpointNotFound.new("Could not connect to #{url}")
-    rescue Timeout::Error => e
+
+    rescue RestClient::RequestTimeout => e
       logger.error loggable.merge(status: 'timeout', error_message: e.message, error_class: e.class.name, end_time: Time.now.to_f).to_json
       raise GdsApi::TimedOutException.new
+
+    rescue RestClient::Exception => e
+      # Log the error here, since we have access to loggable, but raise the
+      # exception up to the calling method to deal with
+      loggable.merge!(status: e.http_code, end_time: Time.now.to_f, body: e.http_body)
+      logger.warn loggable.to_json
+      raise
+
     rescue Errno::ECONNRESET => e
       logger.error loggable.merge(status: 'connection_reset', error_message: e.message, error_class: e.class.name, end_time: Time.now.to_f).to_json
       raise GdsApi::TimedOutException.new
