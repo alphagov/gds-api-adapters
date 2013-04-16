fb3fc6929edf057879d8e843e6bfdb6fc85e42e8
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-04-16 15:27:09 +0100

    Enable the --trace option for publish_gem.
    This should help shed some light on the publishing failures.

--- jenkins.sh
+++ jenkins.sh
@@ -3,4 +3,4 @@ set -e
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 export GOVUK_APP_DOMAIN=dev.gov.uk
 bundle exec rake test
-bundle exec rake publish_gem
+bundle exec rake publish_gem --trace
