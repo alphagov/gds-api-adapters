d4ace58654f2c54b2fbceec73d0071dd2c75fc70
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-06-12 15:42:03 +0100

    Refactor FactCave test helpers to use CommonResponses.

--- lib/gds_api/test_helpers/fact_cave.rb
+++ lib/gds_api/test_helpers/fact_cave.rb
@@ -1,15 +1,19 @@
+require 'gds_api/test_helpers/json_client_helper'
+require 'gds_api/test_helpers/common_responses'
+
 module GdsApi
   module TestHelpers
     module FactCave 
+      include GdsApi::TestHelpers::CommonResponses
 
       FACT_CAVE_ENDPOINT = Plek.current.find('fact-cave')
 
-      def fact_cave_has_a_fact(slug, atts)
-        response = atts.merge({
-          "_response_info" => { "status" => "ok" }
-        })
+      def fact_cave_has_a_fact(slug, value, extra_attrs={})
+        response = fact_for_slug(slug, value).merge(extra_attrs)
 
         stub_request(:get, "#{FACT_CAVE_ENDPOINT}/facts/#{slug}")
+          .to_return(:body => response.to_json, :status => 200)
+        stub_request(:get, "#{FACT_CAVE_ENDPOINT}/facts/#{slug}.json")
           .to_return(:body => response.to_json, :status => 200)
       end
 
@@ -20,6 +24,20 @@ module GdsApi
 
         stub_request(:get, "#{FACT_CAVE_ENDPOINT}/facts/#{slug}")
           .to_return(:body => response.to_json, :status => 404)
+        stub_request(:get, "#{FACT_CAVE_ENDPOINT}/facts/#{slug}.json")
+          .to_return(:body => response.to_json, :status => 404)
+      end
+
+      def fact_for_slug(slug, value = "Sample Value")
+        singular_response_base.merge({
+          "id" => "#{FACT_CAVE_ENDPOINT}/facts/#{slug}",
+          "details" => {
+            "description" => "",
+            "value" => value,
+          },
+          "name" => titleize_slug(slug),
+          "updated_at" => Time.now.utc.xmlschema,
+        })
       end
     end
   end
