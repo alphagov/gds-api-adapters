da7bafb6b9e53fe6bdf7f39f35d1e6509ba6662f
Jake Benilov <benilov@gmail.com>
2013-08-12 09:30:43 +0100

    remove unnecessary include

--- lib/gds_api/support.rb
+++ lib/gds_api/support.rb
@@ -2,8 +2,6 @@ require_relative 'base'
 require_relative 'exceptions'
 
 class GdsApi::Support < GdsApi::Base
-  include GdsApi::ExceptionHandling
-
   def create_foi_request(request_details)
     post_json("#{base_url}/foi_requests", { :foi_request => request_details })
   end
