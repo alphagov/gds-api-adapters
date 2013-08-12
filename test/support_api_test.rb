c5038ca2815eaba8f7292a9b699194ad1b2d46b9
Jake Benilov <benilov@gmail.com>
2013-08-12 09:40:00 +0100

    add missing assertion to support API test

--- test/support_api_test.rb
+++ test/support_api_test.rb
@@ -10,10 +10,12 @@ describe GdsApi::Support do
   it "can create an asset with a file" do
     request_details = {"foi_request"=>{"requester"=>{"name"=>"A", "email"=>"a@b.com"}, "details"=>"abc"}}
 
-    stub_request(:post, "#{@base_api_url}/foi_requests").
+    stub_post = stub_request(:post, "#{@base_api_url}/foi_requests").
       with(:body => {"foi_request" => request_details}.to_json).
       to_return(:status => 201)
 
     @api.create_foi_request(request_details)
+
+    assert_requested(stub_post)
   end
 end
