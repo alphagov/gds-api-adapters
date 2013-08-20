eb4ff9595a35fcc58766e598ae2646dd67e269af
Jake Benilov <benilov@gmail.com>
2013-08-20 22:31:46 +0000

    support: use more appropriate webmock calls

--- lib/gds_api/test_helpers/support.rb
+++ lib/gds_api/test_helpers/support.rb
@@ -4,16 +4,14 @@ module GdsApi
       SUPPORT_ENDPOINT = Plek.current.find('support')
 
       def stub_support_foi_request_creation(request_details = nil)
-        body_expectation = {"foi_request" => request_details}.to_json
-
-        post_stub = stub_request(:post, "#{SUPPORT_ENDPOINT}/foi_requests")
-        post_stub.with(:body => {"foi_request" => request_details}.to_json) unless request_details.nil?
+        post_stub = stub_http_request(:post, "#{SUPPORT_ENDPOINT}/foi_requests")
+        post_stub.with(:body => {"foi_request" => request_details}) unless request_details.nil?
         post_stub.to_return(:status => 201)
       end
 
       def stub_support_problem_report_creation(request_details = nil)
-        post_stub = stub_request(:post, "#{SUPPORT_ENDPOINT}/problem_reports")
-        post_stub.with(:body => {"problem_report" => request_details}.to_json) unless request_details.nil?
+        post_stub = stub_http_request(:post, "#{SUPPORT_ENDPOINT}/problem_reports")
+        post_stub.with(:body => { problem_report: request_details }) unless request_details.nil?
         post_stub.to_return(:status => 201)
       end
 
