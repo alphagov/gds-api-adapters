f69e066b653accfaa263987f4acbd0f0f963c4af
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2014-02-06 17:59:24 +0000

    Move common helper code into business_support_helper

--- lib/gds_api/test_helpers/business_support_api.rb
+++ lib/gds_api/test_helpers/business_support_api.rb
@@ -1,35 +1,20 @@
-require 'gds_api/test_helpers/json_client_helper'
-require 'cgi'
-require 'gds_api/test_helpers/common_responses'
+require 'gds_api/test_helpers/business_support_helper'
 
 module GdsApi
   module TestHelpers
     module BusinessSupportApi
       include GdsApi::TestHelpers::CommonResponses
+      include GdsApi::TestHelpers::BusinessSupportHelper
       # Generally true. If you are initializing the client differently,
       # you could redefine/override the constant or stub directly.
       BUSINESS_SUPPORT_API_ENDPOINT = Plek.current.find('business-support-api')
 
       def setup_business_support_api_schemes_stubs
-        @stubbed_business_support_schemes = {}
-        stub_request(:get, %r{\A#{BUSINESS_SUPPORT_API_ENDPOINT}/business-support-schemes\.json}).to_return do |request|
-          if request.uri.query_values
-            key = facet_key(request.uri.query_values)
-            results = @stubbed_business_support_schemes[key] || []
-          else
-            results = @stubbed_business_support_schemes['default']
-          end
-          {:body => plural_response_base.merge("results" => results, "total" => results.size).to_json}
-        end
-
+        setup_business_support_stubs(BUSINESS_SUPPORT_API_ENDPOINT, 'business-support-schemes')
       end
 
       def business_support_api_has_scheme(scheme, facets={})
-        key = facet_key(facets)
-        unless @stubbed_business_support_schemes.has_key?(key)
-          @stubbed_business_support_schemes[key] = []
-        end
-        @stubbed_business_support_schemes[key] << scheme
+        api_has_business_support(scheme, facets)
       end
 
       def business_support_api_has_schemes(schemes, facets={})
@@ -43,14 +28,6 @@ module GdsApi
         stub_request(:get, %r{\A#{BUSINESS_SUPPORT_API_ENDPOINT}/business-support-schemes/#{slug}\.json}).to_return do |request|
           {:body => response_base.merge(:format => 'business_support', :title => title, :details => scheme).to_json}
         end
-      end
-
-      private
-
-      def facet_key(facets)
-        key = 'default'
-        key = facets.values.sort.hash.to_s if facets and !facets.empty?
-        key
       end
     end
   end
