830a63b1c1dd42c38c843640a932d4a3d8e1588c
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2014-03-12 13:14:48 +0000

    Be explicit when PUTting to the link tracker API
    We want to ensure all errors are bubbled up to the client.

--- lib/gds_api/external_link_tracker.rb
+++ lib/gds_api/external_link_tracker.rb
@@ -2,6 +2,6 @@ require_relative 'base'
 
 class GdsApi::ExternalLinkTracker < GdsApi::Base
   def add_external_link(url)
-    put_json("#{endpoint}/url?url=#{CGI.escape(url)}", {})
+    put_json!("#{endpoint}/url?url=#{CGI.escape(url)}", {})
   end
 end
