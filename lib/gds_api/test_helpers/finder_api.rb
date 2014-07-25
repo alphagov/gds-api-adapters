2ade5189164ad469815f3c3b565352547445762f
Tom Russell <tom@cishub.co.uk>
2014-07-25 12:03:14 +0100

    Revert "Add AAIB and IDF schemas to TestHelpers::FinderApi"

--- lib/gds_api/test_helpers/finder_api.rb
+++ lib/gds_api/test_helpers/finder_api.rb
@@ -5,29 +5,16 @@ module GdsApi
     module FinderApi
       FINDER_API_ENDPOINT = Plek.current.find('finder-api')
 
-      def finder_api_has_schema(finder_slug)
+      def finder_api_has_schema(finder_slug, schema_fixture = FinderApi.schema_fixture)
         stub_request(:get, "#{FINDER_API_ENDPOINT}/finders/#{finder_slug}/schema.json")
           .with(:headers => {'Content-Type'=>'application/json'})
-          .to_return(:status => 200, :body => schema_fixture(finder_slug))
+          .to_return(:status => 200, :body => schema_fixture)
       end
 
-    private
-      def schema_fixture(finder_slug)
-        case finder_slug
-        when "aaib-reports"
-          schema_content("aaib-report-schema.json")
-        when "international-development-funds"
-          schema_content('international-development-funding-schema.json')
-        when "cma-cases"
-          schema_content('cma-case-schema.json')
-        else raise "Unknown schema type: #{schema_type}"
-        end
-      end
-
-      def schema_content(filename)
+      def self.schema_fixture
         File.read(
           File.expand_path(
-            "../../../../test/fixtures/finder_api/#{filename}",
+            "../../../../test/fixtures/finder_api/cma-case-schema.json",
             __FILE__
           )
         )
