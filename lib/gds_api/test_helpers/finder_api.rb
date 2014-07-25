59a899de16f2ac70d97e23f4aca1f83b3140a95b
Tom Russell <tom@cishub.co.uk>
2014-07-25 10:46:43 +0100

    Add AAIB and IDF schemas to TestHelpers::FinderApi
    Previously, the finder_api_has_schema method would either default to
    using its internal CMA case schema (which was outdated) or allow a
    schema to be passed in.
    
    Given that the test helpers already had a fixture for CMA cases, I
    decided to add it here rather than add *another* place that needs
    schemas included.

--- lib/gds_api/test_helpers/finder_api.rb
+++ lib/gds_api/test_helpers/finder_api.rb
@@ -5,16 +5,29 @@ module GdsApi
     module FinderApi
       FINDER_API_ENDPOINT = Plek.current.find('finder-api')
 
-      def finder_api_has_schema(finder_slug, schema_fixture = FinderApi.schema_fixture)
+      def finder_api_has_schema(finder_slug)
         stub_request(:get, "#{FINDER_API_ENDPOINT}/finders/#{finder_slug}/schema.json")
           .with(:headers => {'Content-Type'=>'application/json'})
-          .to_return(:status => 200, :body => schema_fixture)
+          .to_return(:status => 200, :body => schema_fixture(finder_slug))
       end
 
-      def self.schema_fixture
+    private
+      def schema_fixture(finder_slug)
+        case finder_slug
+        when "aaib-reports"
+          schema_content("aaib-report-schema.json")
+        when "international-development-funds"
+          schema_content('international-development-funding-schema.json')
+        when "cma-cases"
+          schema_content('cma-case-schema.json')
+        else raise "Unknown schema type: #{schema_type}"
+        end
+      end
+
+      def schema_content(filename)
         File.read(
           File.expand_path(
-            "../../../../test/fixtures/finder_api/cma-case-schema.json",
+            "../../../../test/fixtures/finder_api/#{filename}",
             __FILE__
           )
         )
