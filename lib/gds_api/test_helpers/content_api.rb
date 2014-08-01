a974066fa736fe835975f893ec4f2fa897c24aa9
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-08-01 15:32:02 +0100

    Require webmock in content api test helpers.
    This was previously relying on the host app having
    mixed in webmock.

--- lib/gds_api/test_helpers/content_api.rb
+++ lib/gds_api/test_helpers/content_api.rb
@@ -2,12 +2,14 @@ require 'gds_api/test_helpers/json_client_helper'
 require 'cgi'
 require 'gds_api/test_helpers/common_responses'
 require 'gds_api/test_helpers/business_support_helper'
+require 'webmock'
 
 module GdsApi
   module TestHelpers
     module ContentApi
       include GdsApi::TestHelpers::CommonResponses
       include GdsApi::TestHelpers::BusinessSupportHelper
+      include WebMock::API
       # Generally true. If you are initializing the client differently,
       # you could redefine/override the constant or stub directly.
       CONTENT_API_ENDPOINT = Plek.current.find('contentapi')
