c8c76079c667b30aeb4955c0abd8765c26b787e4
Jordan Hatch <jordan@jordanh.net>
2014-11-12 14:32:11 +0000

    Support 'start' and 'count' params in Collections API
    This changes the Collections API adapter and test helper to support 
    the 'start' and 'count' parameters, which are used for pagination.

--- lib/gds_api/collections_api.rb
+++ lib/gds_api/collections_api.rb
@@ -3,13 +3,13 @@ require_relative 'exceptions'
 
 class GdsApi::CollectionsApi < GdsApi::Base
 
-  def topic(base_path)
-    get_json(collections_api_url(base_path))
+  def topic(base_path, options={})
+    get_json(collections_api_url(base_path, options))
   end
 
 private
 
-  def collections_api_url(base_path)
-    "#{endpoint}/specialist-sectors#{base_path}"
+  def collections_api_url(base_path, options={})
+    "#{endpoint}/specialist-sectors#{base_path}#{query_string(options)}"
   end
 end
