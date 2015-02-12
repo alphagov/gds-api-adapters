610f777bcb5337a89add01ee5bbcb0e431eede4c
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-02-12 15:51:05 +0000

    Run tests under several ruby versions.
    To ensure the code works for all.

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
 set -e
 rm -f Gemfile.lock
-bundle install --path "${HOME}/bundles/${JOB_NAME}"
+
 export GOVUK_APP_DOMAIN=dev.gov.uk
-bundle exec rake test
+
+for version in 2.2 2.1 1.9.3; do
+  export RBENV_VERSION=$version
+  echo "Running tests under ruby $version"
+  bundle install --path "${HOME}/bundles/${JOB_NAME}"
+  bundle exec rake
+done
+
+unset RBENV_VERSION
+
 if [[ -n "$PUBLISH_GEM" ]]; then
   bundle exec rake publish_gem --trace
 fi
