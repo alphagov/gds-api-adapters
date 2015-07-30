a90d053d41c305e6b2f4b35c3207140b70ab47de
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-07-30 12:38:17 +0100

    Generalise request matching in PublishingApi assertions
    The assertions provided by the PublishingApi test helpers are useful.
    The existing implementation allows an assertion to require that a
    request was made to a publishing api endpoint with a hash containing a
    certain list of required attributes.
    
    However the underlying matching process would only take into account the
    outermost level of the hash when performing the comparison. This means
    that you could make an flexible assertion about the required elements at
    the top level of a document sent to the publishing api, but if you
    wanted to make assertions about the details section or elements nested
    within the details section, you could only perform an exact match on the
    details hash.
    
    This would lead to brittle, difficult to maintain tests.
    
    This commit allows passing custom matcher predicates to the assertions
    and defines a new matcher which can match nested structures more
    loosely.
    
    The original strict matching behaviour is retained as a default to avoid
    possible unintended side-effects on the test suites of other apps.
    
    We considered making the looser behaviour the default, but after
    reviewing the usage of some other apps I had enough doubt about that I
    thought it would be better to be conservative and avoid changing the
    default matching behaviour.
    
    To use the assertions with the looser matcher you can do the following:
    
    ```
    assert_publishing_api_put_item(
      base_path,
      request_json_including(details: {title: "My title"})
    )
    ```
    
    Note that both matchers will convert symbols to strings so the
    hash can use either symbol or string keys.

--- lib/gds_api/test_helpers/publishing_api.rb
+++ lib/gds_api/test_helpers/publishing_api.rb
@@ -43,36 +43,71 @@ module GdsApi
         stub_request(:put, %r{\A#{PUBLISHING_API_ENDPOINT}/publish-intent})
       end
 
-      def assert_publishing_api_put_item(base_path, attributes = {}, times = 1)
+      def assert_publishing_api_put_item(base_path, attributes_or_matcher = {}, times = 1)
         url = PUBLISHING_API_ENDPOINT + "/content" + base_path
-        assert_publishing_api_put(url, attributes, times)
+        assert_publishing_api_put(url, attributes_or_matcher, times)
       end
 
-      def assert_publishing_api_put_draft_item(base_path, attributes = {}, times = 1)
+      def assert_publishing_api_put_draft_item(base_path, attributes_or_matcher = {}, times = 1)
         url = PUBLISHING_API_ENDPOINT + "/draft-content" + base_path
-        assert_publishing_api_put(url, attributes, times)
+        assert_publishing_api_put(url, attributes_or_matcher, times)
       end
 
-      def assert_publishing_api_put_intent(base_path, attributes = {}, times = 1)
+      def assert_publishing_api_put_intent(base_path, attributes_or_matcher = {}, times = 1)
         url = PUBLISHING_API_ENDPOINT + "/publish-intent" + base_path
-        assert_publishing_api_put(url, attributes, times)
+        assert_publishing_api_put(url, attributes_or_matcher, times)
       end
 
-      def assert_publishing_api_put(url, attributes = {}, times = 1)
-        if attributes.empty?
-          assert_requested(:put, url, times: times)
+      def assert_publishing_api_put(url, attributes_or_matcher = {}, times = 1)
+        if attributes_or_matcher.is_a?(Hash)
+          matcher = attributes_or_matcher.empty? ? nil : request_json_matching(attributes_or_matcher)
         else
-          assert_requested(:put, url, times: times) do |req|
-            data = JSON.parse(req.body)
-            attributes.to_a.all? do |key, value|
-              data[key.to_s] == value
-            end
-          end
+          matcher = attributes_or_matcher
+        end
+
+        if matcher
+          assert_requested(:put, url, times: times, &matcher)
+        else
+          assert_requested(:put, url, times: times)
+        end
+      end
+
+      def request_json_matching(required_attributes)
+        ->(request) do
+          data = JSON.parse(request.body)
+          required_attributes.to_a.all? { |key, value| data[key.to_s] == value }
+        end
+      end
+
+      def request_json_including(required_attributes)
+        ->(request) do
+          data = JSON.parse(request.body)
+          values_match_recursively(required_attributes, data)
         end
       end
 
       def publishing_api_isnt_available
         stub_request(:any, /#{PUBLISHING_API_ENDPOINT}\/.*/).to_return(:status => 503)
+      end
+
+    private
+      def values_match_recursively(expected_value, actual_value)
+        case expected_value
+        when Hash
+          return false unless actual_value.is_a?(Hash)
+          expected_value.all? do |expected_sub_key, expected_sub_value|
+            actual_value.has_key?(expected_sub_key.to_s) &&
+              values_match_recursively(expected_sub_value, actual_value[expected_sub_key.to_s])
+          end
+        when Array
+          return false unless actual_value.is_a?(Array)
+          return false unless actual_value.size == expected_value.size
+          expected_value.each.with_index.all? do |expected_sub_value, i|
+            values_match_recursively(expected_sub_value, actual_value[i])
+          end
+        else
+          expected_value == actual_value
+        end
       end
     end
   end
