ff568f797e47bd86e06e38cec1e65476fe7cb55b
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-08-12 16:11:12 +0100

    Fix `helpers.rb` after finder api removal
    The removal of `finder_api` in 2715160c539 did not remove the `require`
    line from `helpers.rb`, so the file raises an error when required.
    
    There were no tests which load this file, so this error was not picked
    up.
    
    I've removed the offending require line and added a very simple test of
    the `helpers.rb` so that such errors are picked up in the future.

---
+++ test/helpers_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+require 'gds_api/helpers'
+
+describe GdsApi::Helpers do
+  class TestDouble
+    include GdsApi::Helpers
+  end
+
+  it "should define helpers for the various apis" do
+    test_with_helpers = TestDouble.new
+
+    refute_nil test_with_helpers.asset_manager_api
+    refute_nil test_with_helpers.business_support_api
+    refute_nil test_with_helpers.content_api
+    refute_nil test_with_helpers.content_register
+    refute_nil test_with_helpers.content_store
+    refute_nil test_with_helpers.fact_cave_api
+    refute_nil test_with_helpers.publisher_api
+    refute_nil test_with_helpers.imminence_api
+    refute_nil test_with_helpers.licence_application_api
+    refute_nil test_with_helpers.need_api
+    refute_nil test_with_helpers.panopticon_api
+    refute_nil test_with_helpers.worldwide_api
+    refute_nil test_with_helpers.email_alert_api
+  end
+end
