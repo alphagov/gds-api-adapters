53498ae4492075ac73cc72b6cc4961d66476f5cf
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-11-16 16:37:07 +0000

    Add some test helpers for the Publishing API v2
    This commit adds test helpers for the Publishing API v2. Most of these
    have been copied over from the v1 test helpers and modified.

--- lib/gds_api/test_helpers/publishing_api_v2.rb
+++ lib/gds_api/test_helpers/publishing_api_v2.rb
@@ -18,6 +18,43 @@ module GdsApi
         stub_publishing_api_put(content_id, body, '/links')
       end
 
+      def stub_default_publishing_api_put
+        stub_request(:put, %r{\A#{PUBLISHING_API_V2_ENDPOINT}/content})
+      end
+
+      def assert_publishing_api_put_item(content_id, attributes_or_matcher = {}, times = 1)
+        url = PUBLISHING_API_V2_ENDPOINT + "/content/" + content_id
+        assert_publishing_api_put(url, attributes_or_matcher, times)
+      end
+
+      def assert_publishing_api_put(url, attributes_or_matcher = {}, times = 1)
+        if attributes_or_matcher.is_a?(Hash)
+          matcher = attributes_or_matcher.empty? ? nil : request_json_matching(attributes_or_matcher)
+        else
+          matcher = attributes_or_matcher
+        end
+
+        if matcher
+          assert_requested(:put, url, times: times, &matcher)
+        else
+          assert_requested(:put, url, times: times)
+        end
+      end
+
+      def request_json_matching(required_attributes)
+        ->(request) do
+          data = JSON.parse(request.body)
+          required_attributes.to_a.all? { |key, value| data[key.to_s] == value }
+        end
+      end
+
+      def request_json_including(required_attributes)
+        ->(request) do
+          data = JSON.parse(request.body)
+          required_attributes == data
+        end
+      end
+
     private
       def stub_publishing_api_put(content_id, body, resource_path)
         url = PUBLISHING_API_V2_ENDPOINT + resource_path + "/" + content_id
