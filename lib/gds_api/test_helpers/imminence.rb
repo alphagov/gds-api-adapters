34be7baf9bc798149edec3c0687649885dd9830d
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-02-25 12:28:42 +0000

    Add places for postcode helper for imminence
    - Helper methods added to enable the stubbing of find my nearest place searches via Imminence
    - Small refactor to make use of the new helper methods

--- lib/gds_api/test_helpers/imminence.rb
+++ lib/gds_api/test_helpers/imminence.rb
@@ -8,11 +8,8 @@ module GdsApi
       IMMINENCE_API_ENDPOINT = Plek.current.find('imminence')
 
       def imminence_has_places(latitude, longitude, details)
-        response = JSON.dump(details['details'])
-
-        stub_request(:get, "#{IMMINENCE_API_ENDPOINT}/places/#{details['slug']}.json").
-        with(:query => {"lat" => latitude, "lng" => longitude, "limit" => "5"}).
-        to_return(:status => 200, :body => response, :headers => {})
+        query_hash = { "lat" => latitude, "lng" => longitude, "limit" => "5" }
+        stub_imminence_places_request(details['slug'], query_hash, details['details'])
       end
 
       def imminence_has_areas_for_postcode(postcode, areas)
@@ -24,6 +21,16 @@ module GdsApi
 
         stub_request(:get, %r{\A#{IMMINENCE_API_ENDPOINT}/areas/#{postcode}\.json}).
           to_return(:body => results.to_json)
+
+      def imminence_has_places_for_postcode(places, slug, postcode, limit)
+        query_hash = { "postcode" => postcode, "limit" => limit }
+        stub_imminence_places_request(slug, query_hash, places)
+      end
+
+      def stub_imminence_places_request(slug, query_hash, return_data, status_code = 200)
+        stub_request(:get, "#{IMMINENCE_API_ENDPOINT}/places/#{slug}.json").
+        with(:query => query_hash).
+        to_return(:status => status_code, :body => return_data.to_json, :headers => {})
       end
     end
   end
