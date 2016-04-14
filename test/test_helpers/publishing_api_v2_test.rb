b4754a9821de4f4d971813a06105a2c99eac19a2
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2016-04-14 10:03:48 +0000

    Add test for publishing_api_has_content

--- test/test_helpers/publishing_api_v2_test.rb
+++ test/test_helpers/publishing_api_v2_test.rb
@@ -16,4 +16,14 @@ describe GdsApi::TestHelpers::PublishingApiV2 do
       assert_equal publishing_api.lookup_content_id(base_path: "/foo"), "2878337b-bed9-4e7f-85b6-10ed2cbcd504"
     end
   end
+
+  describe "#publishing_api_has_content" do
+    it "stubs the call to get content items" do
+      publishing_api_has_content([{"content_id" => "2878337b-bed9-4e7f-85b6-10ed2cbcd504"}])
+
+      response = publishing_api.get_content_items({})['results']
+
+      assert_equal([{ "content_id" => "2878337b-bed9-4e7f-85b6-10ed2cbcd504" }], response)
+    end
+  end
 end
