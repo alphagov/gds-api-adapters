ae2c271e7073ed0e5a7d884dc9147e8d5e0f0e61
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2016-04-15 10:15:23 +0100

    Write tests for current GovukHeaders behaviour.

---
+++ test/govuk_headers_test.rb
@@ -0,0 +1,18 @@
+require_relative "test_helper"
+require "gds_api/govuk_headers"
+
+describe GdsApi::GovukHeaders do
+  before :each do
+    Thread.current[:headers] = nil if Thread.current[:headers]
+  end
+
+  it "supports read/write of headers" do
+    GdsApi::GovukHeaders.set_header("GDS-Request-Id", "123-456")
+    GdsApi::GovukHeaders.set_header("Content-Type", "application/pdf")
+
+    assert_equal({
+      "GDS-Request-Id" => "123-456",
+      "Content-Type" => "application/pdf",
+    }, GdsApi::GovukHeaders.headers)
+  end
+end
