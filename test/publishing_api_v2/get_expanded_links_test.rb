a351ad15a245c5fe843f1d0a820404d7fcbf5d8c
Tijmen Brommet <tijmen@gmail.com>
2016-05-17 10:23:18 +0100

    Add support for get_expanded_links endpoint
    This endpoint was added in
    https://github.com/alphagov/publishing-api/pull/307

---
+++ test/publishing_api_v2/get_expanded_links_test.rb
@@ -0,0 +1,85 @@
+require 'test_helper'
+require 'gds_api/publishing_api_v2'
+require 'json'
+
+describe GdsApi::PublishingApiV2 do
+  include PactTest
+
+  before do
+    @api_client = GdsApi::PublishingApiV2.new('http://localhost:3093')
+    @content_id = "bed722e6-db68-43e5-9079-063f623335a7"
+  end
+
+  describe "#get_expanded_links" do
+    it "responds with the links when there's a links entry with links" do
+      publishing_api
+        .given("organisation links exist for content_id #{@content_id}")
+        .upon_receiving("a get-expanded-links request")
+        .with(
+          method: :get,
+          path: "/v2/expanded-links/#{@content_id}",
+        )
+        .will_respond_with(
+          status: 200,
+          body: {
+            expanded_links: {
+              organisations: [
+                { content_id: "20583132-1619-4c68-af24-77583172c070" }
+              ]
+            }
+          }
+        )
+
+      response = @api_client.get_expanded_links(@content_id)
+
+      expected_body = {
+        "expanded_links" => {
+          "organisations" => [
+            { "content_id" => "20583132-1619-4c68-af24-77583172c070" }
+          ]
+        }
+      }
+      assert_equal 200, response.code
+      assert_equal expected_body, response.to_h
+    end
+
+    it "responds with the empty thing set if there's an empty link set" do
+      publishing_api
+        .given("empty links exist for content_id #{@content_id}")
+        .upon_receiving("a get-expanded-links request")
+        .with(
+          method: :get,
+          path: "/v2/expanded-links/#{@content_id}",
+        )
+        .will_respond_with(
+          status: 200,
+          body: {
+            expanded_links: {
+            }
+          }
+        )
+
+      response = @api_client.get_expanded_links(@content_id)
+
+      assert_equal 200, response.code
+      assert_equal({}, response.to_h['expanded_links'])
+    end
+
+    it "responds with 404 if there's no link set entry" do
+      publishing_api
+        .given("no links exist for content_id #{@content_id}")
+        .upon_receiving("a get-expanded-links request")
+        .with(
+          method: :get,
+          path: "/v2/expanded-links/#{@content_id}",
+        )
+        .will_respond_with(
+          status: 404
+        )
+
+      response = @api_client.get_expanded_links(@content_id)
+
+      assert_nil response
+    end
+  end
+end
