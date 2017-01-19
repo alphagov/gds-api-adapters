846ca044c85c9ad797327e6300f87d28f017f824
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2017-01-19 17:36:52 +0000

    Add test coverage for #publishing_api_has_item

--- test/test_helpers/publishing_api_v2_test.rb
+++ test/test_helpers/publishing_api_v2_test.rb
@@ -119,6 +119,14 @@ describe GdsApi::TestHelpers::PublishingApiV2 do
     end
   end
 
+  describe "#publishing_api_has_item" do
+    it "stubs the call to get content items" do
+      publishing_api_has_item("content_id" => "2878337b-bed9-4e7f-85b6-10ed2cbcd504")
+      response = publishing_api.get_content("2878337b-bed9-4e7f-85b6-10ed2cbcd504").parsed_content
+
+      assert_equal({ "content_id" => "2878337b-bed9-4e7f-85b6-10ed2cbcd504" }, response)
+    end
+  end
   describe "#publishing_api_has_expanded_links" do
     it "stubs the call to get expanded links" do
       payload = {
