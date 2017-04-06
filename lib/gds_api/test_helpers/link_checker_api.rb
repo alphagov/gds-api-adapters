bf7618ba19ef1c7f8c698d260ed56ec3c8a64869
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-04-06 15:07:53 +0000

    Add adapters for the Link Checker API

---
+++ lib/gds_api/test_helpers/link_checker_api.rb
@@ -0,0 +1,70 @@
+require "gds_api/test_helpers/json_client_helper"
+
+module GdsApi
+  module TestHelpers
+    module LinkCheckerApi
+      LINK_CHECKER_API_ENDPOINT = Plek.current.find("link-checker-api")
+
+      def link_checker_api_link_report_hash(uri:, status: :ok, checked: nil, errors: {}, warnings: {})
+        {
+          uri: uri,
+          status: status,
+          checked: checked || Time.now.iso8601,
+          errors: errors,
+          warnings: warnings,
+        }
+      end
+
+      def link_checker_api_batch_report_hash(id:, status: :completed, links: [], totals: {}, completed_at: nil)
+        {
+          id: id,
+          status: status,
+          links: links.map { |hash| link_checker_api_link_report_hash(**hash) },
+          totals: totals,
+          completed_at: completed_at || Time.now.iso8601,
+        }
+      end
+
+      def link_checker_api_check(uri:, status: :ok, checked: nil, errors: {}, warnings: {})
+        body = link_checker_api_link_report_hash(
+          uri: uri, status: status, checked: checked, errors: errors, warnings: warnings
+        ).to_json
+
+        stub_request(:get, "#{LINK_CHECKER_API_ENDPOINT}/check")
+          .with(query: { uri: uri })
+          .to_return(body: body, status: 200, headers: { "Content-Type" => "application/json" })
+      end
+
+      def link_checker_api_get_batch(id:, status: :completed, links: [], totals: {}, completed_at: nil)
+        body = link_checker_api_batch_report_hash(
+          id: id, status: status, links: links, totals: totals, completed_at: completed_at
+        ).to_json
+
+        stub_request(:get, "#{LINK_CHECKER_API_ENDPOINT}/batch/#{id}")
+          .to_return(body: body, status: 200, headers: { "Content-Type" => "application/json" })
+      end
+
+      def link_checker_api_create_batch(uris:, checked_within: nil, webhook_uri: nil, id: 0, status: :in_progress, links: nil, totals: {}, completed_at: nil)
+        links = uris.map { |uri| { uri: uri } } if links.nil?
+
+        response_body = link_checker_api_batch_report_hash(
+          id: id, status: status, links: links, totals: totals, completed_at: completed_at
+        ).to_json
+
+        request_body = {
+          uris: uris,
+          checked_within: checked_within,
+          webhook_uri: webhook_uri,
+        }.delete_if { |_, v| v.nil? }.to_json
+
+        stub_request(:post, "#{LINK_CHECKER_API_ENDPOINT}/batch")
+          .with(body: request_body)
+          .to_return(
+            body: response_body,
+            status: status == :in_progress ? 202 : 201,
+            headers: { "Content-Type" => "application/json" }
+          )
+      end
+    end
+  end
+end
---
+++ test/link_checker_api_test.rb
@@ -0,0 +1,44 @@
+require "test_helper"
+require "gds_api/link_checker_api"
+require "gds_api/test_helpers/link_checker_api"
+
+describe GdsApi::LinkCheckerApi do
+  include GdsApi::TestHelpers::LinkCheckerApi
+
+  before do
+    @base_api_url = Plek.find("link-checker-api")
+    @api = GdsApi::LinkCheckerApi.new(@base_api_url)
+  end
+
+  describe "#check" do
+    it "returns a useful response" do
+      link_checker_api_check(uri: "http://example.com", status: :broken)
+
+      link_report = @api.check("http://example.com")
+
+      assert_equal :broken, link_report.status
+    end
+  end
+
+  describe "#create_batch" do
+    it "returns a useful response" do
+      link_checker_api_create_batch(uris: ["http://example.com"])
+
+      batch_report = @api.create_batch(["http://example.com"])
+
+      assert_equal :in_progress, batch_report.status
+      assert_equal "http://example.com", batch_report.links[0].uri
+    end
+  end
+
+  describe "#get_batch" do
+    it "returns a useful response" do
+      link_checker_api_get_batch(id: 10, links: [{ uri: "http://example.com" }])
+
+      batch_report = @api.get_batch(10)
+
+      assert_equal :completed, batch_report.status
+      assert_equal "http://example.com", batch_report.links[0].uri
+    end
+  end
+end
