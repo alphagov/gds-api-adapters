69e177fb8f3104f0ca609dff492bbfa70b6d296e
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-01-06 14:44:50 +0000

    Declare new pact with Collection's Organisation API Move existing tests for organisations_api

--- test/test_helpers/pact_helper.rb
+++ test/test_helpers/pact_helper.rb
@@ -1,13 +1,26 @@
 PUBLISHING_API_PORT = 3001
+ORGANISATION_API_PORT = 3002
 
 def publishing_api_host
   "http://localhost:#{PUBLISHING_API_PORT}"
+end
+
+def organisation_api_host
+  "http://localhost:#{ORGANISATION_API_PORT}"
 end
 
 Pact.service_consumer "GDS API Adapters" do
   has_pact_with "Publishing API" do
     mock_service :publishing_api do
       port PUBLISHING_API_PORT
+    end
+  end
+end
+
+Pact.service_consumer "GDS API Adapters" do
+  has_pact_with "Collections Organisation API" do
+    mock_service :organisation_api do
+      port ORGANISATION_API_PORT
     end
   end
 end
