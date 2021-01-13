1ac69acc4f12d5ff863ec7af843eaf4168e8716a
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-01-13 15:14:14 +0000

    Test that the organisations end point returns a list of organisations
    Running bundle exec rake TEST=test/organisations_api_test.rb produces a pact file which
     is stored in spec/pacts/gds_api_adapters-collections_organisation_api.json
    - spec/pacts is on the git ignore, because we use pact broker to store the pactfile
    - In a follow up piece of work, this file will be sent to the pact broker.

--- test/organisations_api_test.rb
+++ test/organisations_api_test.rb
@@ -5,5 +5,59 @@ require "gds_api/test_helpers/organisations"
 describe GdsApi::Organisations do
   include GdsApi::TestHelpers::Organisations
   include PactTest
-  
+
+  def base_api_url
+    Plek.new.website_root
+  end
+
+  def api_client
+    @api_client ||= GdsApi::Organisations.new(organisation_api_host)
+  end
+
+  def organisation(slug: "test-department")
+    {
+      "id" => Pact.like("www.gov.uk/api/organisations/#{slug}"),
+      "title" => Pact.like("Test Department"),
+      "updated_at" => Pact.like("2019-05-15T12:12:17.000+01:00"),
+      "web_url" => Pact.like("www.gov.uk/government/organisations/#{slug}"),
+      "details" => {
+        "slug" => Pact.like(slug),
+        "content_id" => Pact.like("b854f170-53c8-4098-bf77-e8ef42f93107"),
+      },
+      "analytics_identifier" => Pact.like("OT1276"),
+      "child_organisations" => [],
+      "superseded_organisations" => [],
+    }
+  end
+
+  describe "fetching list of organisations" do
+    before do
+      organisation_api
+        .given("there is a list of organisations")
+        .upon_receiving("a request for the organisation list")
+        .with(
+          method: :get,
+          path: "/api/organisations",
+          headers: GdsApi::JsonClient.default_request_headers,
+        )
+        .will_respond_with(
+          status: 200,
+          body: {
+            results: [
+              organisation,
+              organisation,
+            ],
+          },
+          headers: {
+            "Content-Type" => "application/json; charset=utf-8",
+          },
+        )
+    end
+
+    it "responds with 200 OK and a list of organisations" do
+      response = api_client.organisations
+      assert_equal 2, response["results"].count
+      assert_equal 200, response.code
+    end
+  end
 end
