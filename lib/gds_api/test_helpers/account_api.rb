1f8ee57b70729a9f591bbc748253efed0d6d9d1e
Michael Walker <mike@barrucadu.co.uk>
2021-05-06 09:51:15 +0100

    Add 401 and 403 test helpers for account-api methods

--- lib/gds_api/test_helpers/account_api.rb
+++ lib/gds_api/test_helpers/account_api.rb
@@ -56,11 +56,51 @@ module GdsApi
         )
       end
 
-      def stub_account_api_set_email_subscription(slug: "slug", **options)
+      def stub_account_api_unauthorized_get_email_subscription(**options)
+        stub_account_api_request(
+          :get,
+          "/api/transition-checker-email-subscription",
+          response_status: 401,
+          **options,
+        )
+      end
+
+      def stub_account_api_forbidden_get_email_subscription(needed_level_of_authentication: "level1", **options)
+        stub_account_api_request(
+          :get,
+          "/api/transition-checker-email-subscription",
+          response_status: 403,
+          response_body: { needed_level_of_authentication: needed_level_of_authentication },
+          **options,
+        )
+      end
+
+      def stub_account_api_set_email_subscription(slug: nil, **options)
         stub_account_api_request(
           :post,
           "/api/transition-checker-email-subscription",
           with: { body: hash_including({ slug: slug }.compact) },
+          **options,
+        )
+      end
+
+      def stub_account_api_unauthorized_set_email_subscription(slug: nil, **options)
+        stub_account_api_request(
+          :post,
+          "/api/transition-checker-email-subscription",
+          with: { body: hash_including({ slug: slug }.compact) },
+          response_status: 401,
+          **options,
+        )
+      end
+
+      def stub_account_api_forbidden_set_email_subscription(slug: nil, needed_level_of_authentication: "level1", **options)
+        stub_account_api_request(
+          :post,
+          "/api/transition-checker-email-subscription",
+          with: { body: hash_including({ slug: slug }.compact) },
+          response_status: 403,
+          response_body: { needed_level_of_authentication: needed_level_of_authentication },
           **options,
         )
       end
@@ -75,11 +115,53 @@ module GdsApi
         )
       end
 
+      def stub_account_api_unauthorized_has_attributes(attributes: [], **options)
+        querystring = Rack::Utils.build_nested_query({ attributes: attributes }.compact)
+        stub_account_api_request(
+          :get,
+          "/api/attributes?#{querystring}",
+          response_status: 401,
+          **options,
+        )
+      end
+
+      def stub_account_api_forbidden_has_attributes(attributes: [], needed_level_of_authentication: "level1", **options)
+        querystring = Rack::Utils.build_nested_query({ attributes: attributes }.compact)
+        stub_account_api_request(
+          :get,
+          "/api/attributes?#{querystring}",
+          response_status: 403,
+          response_body: { needed_level_of_authentication: needed_level_of_authentication },
+          **options,
+        )
+      end
+
       def stub_account_api_set_attributes(attributes: nil, **options)
         stub_account_api_request(
           :patch,
           "/api/attributes",
           with: { body: hash_including({ attributes: attributes }.compact) },
+          **options,
+        )
+      end
+
+      def stub_account_api_unauthorized_set_attributes(attributes: nil, **options)
+        stub_account_api_request(
+          :patch,
+          "/api/attributes",
+          with: { body: hash_including({ attributes: attributes }.compact) },
+          response_status: 401,
+          **options,
+        )
+      end
+
+      def stub_account_api_forbidden_set_attributes(attributes: nil, needed_level_of_authentication: "level1", **options)
+        stub_account_api_request(
+          :patch,
+          "/api/attributes",
+          with: { body: hash_including({ attributes: attributes }.compact) },
+          response_status: 403,
+          response_body: { needed_level_of_authentication: needed_level_of_authentication },
           **options,
         )
       end
@@ -94,8 +176,30 @@ module GdsApi
         )
       end
 
+      def stub_account_api_unauthorized_get_attributes_names(attributes: [], **options)
+        querystring = Rack::Utils.build_nested_query({ attributes: attributes }.compact)
+        stub_account_api_request(
+          :get,
+          "/api/attributes/names?#{querystring}",
+          response_status: 401,
+          **options,
+        )
+      end
+
+      def stub_account_api_forbidden_get_attributes_names(attributes: [], needed_level_of_authentication: "level1", **options)
+        querystring = Rack::Utils.build_nested_query({ attributes: attributes }.compact)
+        stub_account_api_request(
+          :get,
+          "/api/attributes/names?#{querystring}",
+          response_status: 403,
+          response_body: { needed_level_of_authentication: needed_level_of_authentication },
+          **options,
+        )
+      end
+
       def stub_account_api_request(method, path, with: {}, response_status: 200, response_body: {}, govuk_account_session: nil, new_govuk_account_session: nil)
         with.merge!(headers: { GdsApi::AccountApi::AUTH_HEADER_NAME => govuk_account_session }) if govuk_account_session
+        new_govuk_account_session = nil if response_status >= 400
         to_return = { status: response_status, body: response_body.merge(govuk_account_session: new_govuk_account_session).compact.to_json }
         if with.empty?
           stub_request(method, "#{ACCOUNT_API_ENDPOINT}#{path}").to_return(**to_return)
