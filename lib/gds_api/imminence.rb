2f8a400ff91cd71f8992bbaae2c1d0cca35ae299
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2022-09-29 16:44:30 +0100

    Remove method for imminence /areas/:type endpoint

--- lib/gds_api/imminence.rb
+++ lib/gds_api/imminence.rb
@@ -35,11 +35,6 @@ class GdsApi::Imminence < GdsApi::Base
     get_json(url)
   end
 
-  def areas_for_type(type)
-    url = "#{@endpoint}/areas/#{type}.json"
-    get_json(url)
-  end
-
   # @private
   def self.extract_location_hash(location)
     # Deal with all known location formats:
