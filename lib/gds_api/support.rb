3d27c9827e22b83105ce853bd4039563970675a9
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-01-25 10:39:31 +0000

    Remove 'create_foi_request' API method
    This only appears to be used in gds-api-adapters:
    https://github.com/search?q=org%3Aalphagov+create_foi_request&type=code
    
    It was originally added in https://github.com/alphagov/gds-api-adapters/pull/69,
    to support a feature added to Feedback: https://github.com/alphagov/feedback/pull/65/files
    ...in 2013.
    
    But this was removed from Feedback in 2019:
    https://github.com/alphagov/feedback/commit/9b82531f2dfb2fd1514fe5b09f936b83f73f56d4

--- lib/gds_api/support.rb
+++ lib/gds_api/support.rb
@@ -1,10 +1,6 @@
 require_relative "base"
 
 class GdsApi::Support < GdsApi::Base
-  def create_foi_request(request_details)
-    post_json("#{base_url}/foi_requests", foi_request: request_details)
-  end
-
   def create_named_contact(request_details)
     post_json("#{base_url}/named_contacts", named_contact: request_details)
   end
