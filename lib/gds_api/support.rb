25bac87f05b021cbd229f2b9a27f62ed0f287c97
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-03-22 12:05:53 +0000

    Remove exposed 'Named Contact' functionality from Support app
    This is no longer used as of https://github.com/alphagov/feedback/pull/1835

--- lib/gds_api/support.rb
+++ lib/gds_api/support.rb
@@ -1,10 +1,6 @@
 require_relative "base"
 
 class GdsApi::Support < GdsApi::Base
-  def create_named_contact(request_details)
-    post_json("#{base_url}/named_contacts", named_contact: request_details)
-  end
-
   def feedback_url(slug)
     "#{base_url}/anonymous_feedback?path=#{slug}"
   end
--- test/support_test.rb
+++ test/support_test.rb
@@ -1,31 +1,10 @@
 require "test_helper"
 require "gds_api/support"
-require "gds_api/test_helpers/support"
 
 describe GdsApi::Support do
-  include GdsApi::TestHelpers::Support
-
   before do
     @base_api_url = Plek.find("support")
     @api = GdsApi::Support.new(@base_api_url)
-  end
-
-  it "can create a named contact" do
-    request_details = { certain: "details" }
-
-    stub_post = stub_request(:post, "#{@base_api_url}/named_contacts")
-      .with(body: { "named_contact" => request_details }.to_json)
-      .to_return(status: 201)
-
-    @api.create_named_contact(request_details)
-
-    assert_requested(stub_post)
-  end
-
-  it "throws an exception when the support app isn't available while creating named contacts" do
-    stub_support_isnt_available
-
-    assert_raises(GdsApi::HTTPServerError) { @api.create_named_contact({}) }
   end
 
   it "gets the correct feedback URL" do
