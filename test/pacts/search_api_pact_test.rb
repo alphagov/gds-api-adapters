a869d3e8e3ead8b245f7b8aa77a72ca2525ce0a9
Lucy Stringer <lucy.stringer@digital.cabinet-office.gov.uk>
2026-01-07 14:57:28 +0000

    Add initial pact test We will need multiple pact tests for search, but this PR only adds the most straightforward one so that we can get the pact tests configured correctly.

---
+++ test/pacts/search_api_pact_test.rb
@@ -0,0 +1,55 @@
+require "test_helper"
+require "gds_api/search"
+
+describe "GdsApi::Search pact tests" do
+  include PactTest
+
+  let(:api_client) { GdsApi::Search.new(search_api_host) }
+
+  describe "#search" do
+    it "fetches a search response" do
+      search_api
+        .given("there are search results for universal credit")
+        .upon_receiving("a query for universal credit")
+        .with(
+          method: :get,
+          query: "q=universal+credit",
+          path: "/search.json",
+          headers: GdsApi::JsonClient.default_request_headers,
+        )
+        .will_respond_with(
+          status: 200,
+          body: {
+            results: [
+              search_result,
+              search_result,
+            ],
+            total: Pact.like(2),
+            start: Pact.like(0),
+            aggregates: Pact.like({}),
+            suggested_queries: Pact.like([]),
+            suggested_autocomplete: Pact.like([]),
+            es_cluster: Pact.like("A"),
+          },
+          headers: {
+            "Content-Type" => "application/json",
+          },
+        )
+
+      api_client.search(q: "universal credit")
+    end
+  end
+
+private
+
+  def search_result
+    {
+      "link" => Pact.like("/universal-credit"),
+      "title" => Pact.like("Universal credit"),
+      "index" => Pact.like("government_test"),
+      "_id" => Pact.like("/universal-credit"),
+      "elasticsearch_type" => Pact.like("edition"),
+      "document_type" => Pact.like("edition"),
+    }
+  end
+end
