ad6ce1e1f9b568f84fad78de2152112eb86b73ef
Lucy Stringer <lucy.stringer@digital.cabinet-office.gov.uk>
2026-02-05 15:26:57 +0000

    Add pact test for search with no keyword included

--- test/pacts/search_api_pact_test.rb
+++ test/pacts/search_api_pact_test.rb
@@ -7,7 +7,7 @@ describe "GdsApi::Search pact tests" do
   let(:api_client) { GdsApi::Search.new(search_api_host) }
 
   describe "#search" do
-    it "fetches a search response" do
+    it "fetches a search response when search term is included" do
       search_api
         .given("there are search results for universal credit")
         .upon_receiving("a valid query for universal credit")
@@ -37,6 +37,38 @@ describe "GdsApi::Search pact tests" do
         )
 
       api_client.search(q: "universal credit")
+    end
+
+    it "fetches a search response when no search term is included" do
+      search_api
+        .given("there are search results for universal credit")
+        .upon_receiving("a valid query with no keyword")
+        .with(
+          method: :get,
+          query: "count=2",
+          path: "/search.json",
+          headers: GdsApi::JsonClient.default_request_headers,
+        )
+        .will_respond_with(
+          status: 200,
+          body: {
+            results: [
+              search_result,
+              search_result,
+            ],
+            total: Pact.like(2),
+            start: Pact.like(0),
+            aggregates: Pact.like({}),
+            suggested_queries: Pact.like([]),
+            suggested_autocomplete: Pact.like([]),
+            es_cluster: Pact.like("A"),
+          },
+          headers: {
+            "Content-Type" => "application/json",
+          },
+        )
+
+      api_client.search(count: 2)
     end
 
     it "responds with 422 when ordering field is invalid" do
