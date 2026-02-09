77feffa65c15aae46aa0290c995dcf45fd2f4b9a
Lucy Stringer <lucy.stringer@digital.cabinet-office.gov.uk>
2026-02-09 14:50:33 +0000

    Add pact tests for #search_enum #search_enum allows for the /search endpoint to be called page by page. It uses the /search endpoint, and needs params `start` and `count` to be allowed.

--- test/pacts/search_api_pact_test.rb
+++ test/pacts/search_api_pact_test.rb
@@ -95,6 +95,59 @@ describe "GdsApi::Search pact tests" do
     end
   end
 
+  describe "#search_enum" do
+    it "fetches a search response when no search term is included" do
+      search_api
+        .given("there are search results for universal credit")
+        .upon_receiving("a request for the first batch of results")
+        .with(
+          method: :get,
+          query: "start=0&count=2",
+          path: "/search.json",
+          headers: GdsApi::JsonClient.default_request_headers,
+        )
+        .will_respond_with(
+          status: 200,
+          body: {
+            results: [
+              search_result,
+              search_result,
+            ],
+            total: Pact.like(2),
+            start: Pact.like(0),
+            aggregates: Pact.like({}),
+            suggested_queries: Pact.like([]),
+            suggested_autocomplete: Pact.like([]),
+            es_cluster: Pact.like("A"),
+          },
+        )
+
+      search_api
+        .given("there are search results for universal credit")
+        .upon_receiving("a request for the first batch of results")
+        .with(
+          method: :get,
+          query: "start=2&count=2",
+          path: "/search.json",
+          headers: GdsApi::JsonClient.default_request_headers,
+        )
+        .will_respond_with(
+          status: 200,
+          body: {
+            results: [],
+            total: Pact.like(2),
+            start: Pact.like(2),
+            aggregates: Pact.like({}),
+            suggested_queries: Pact.like([]),
+            suggested_autocomplete: Pact.like([]),
+            es_cluster: Pact.like("A"),
+          },
+        )
+
+      api_client.search_enum({}, page_size: 2).to_a
+    end
+  end
+
 private
 
   def search_result
